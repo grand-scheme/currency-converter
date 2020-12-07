474a9e4a46f5aad8c56f44c823e5b923106bb34e
Shannon Grantski <grantski@pm.me>
2020-12-07 07:08:31 -0800

    save object variables

--- assets/js/conversion.js
+++ assets/js/conversion.js
@@ -1,11 +1,12 @@
 export default class Response {
-  static getElements(input, response) {
-    console.log(response);
-    console.log(response.conversion_rates.AUD);
-    console.log(response.conversion_rates.CAD);
-    console.log(response.conversion_rates.NZD);
-    console.log(response.conversion_rates.BSD);
-    console.log(response.conversion_rates.HKD);
-    alert("You have " + (input * response.conversion_rates.HKD) + "Hong Kong Dollars");
+  static getElements(response) {
+    let currencyObject = {
+      "dollarAU": response.conversion_rates.AUD,
+      "dollarBS": response.conversion_rates.BSD,
+      "dollarCA": response.conversion_rates.CAD,
+      "dollarHK": response.conversion_rates.HKD,
+      "dollarNZ": response.conversion_rates.NZD
+    };
+    return currencyObject;
   }
 }
