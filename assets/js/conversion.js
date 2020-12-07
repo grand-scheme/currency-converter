d49f7de82a6133cbc02056e8a7e94eeb5c7c2297
Shannon Grantski <grantski@pm.me>
2020-12-07 08:16:07 -0800

    tie currency to search input

--- assets/js/conversion.js
+++ assets/js/conversion.js
@@ -1,12 +1,21 @@
 export default class Response {
-  static getElements(response) {
-    let currencyObject = {
-      "dollarAU": response.conversion_rates.AUD,
-      "dollarBS": response.conversion_rates.BSD,
-      "dollarCA": response.conversion_rates.CAD,
-      "dollarHK": response.conversion_rates.HKD,
-      "dollarNZ": response.conversion_rates.NZD
-    };
-    return currencyObject;
+  static getElements(response, searchInput) {
+    let currencyObject = response.conversion_rates;
+    let result = "";
+
+    if (searchInput === "Australian Dollars") {
+      result = currencyObject.AUD;
+    } else if (searchInput === "Bahamian Dollars") {
+      result = currencyObject.BSD;
+    } else if (searchInput === "Canadian Dollars") {
+      result = currencyObject.CAD;
+    } else if (searchInput === "Hong Kong Dollars") {
+      result = currencyObject.HKD;
+    } else if (searchInput === "New Zealand Dollars") {
+      result = currencyObject.NZD;
+    } else {
+      console.log("different currency");
+    }
+    return result;
   }
 }
