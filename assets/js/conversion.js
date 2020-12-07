c2b486e2b42b1a23712bd9ef963b95a4d5b472d0
Shannon Grantski <grantski@pm.me>
2020-12-07 13:41:05 -0800

    update variables for clarity

--- assets/js/conversion.js
+++ assets/js/conversion.js
@@ -1,34 +1,35 @@
 export default class Response {
-  static getCurrency(searchInput) {
+  static getCurrency(userCurrency) {
     let result = "";
-    if (searchInput === "Australian Dollars") {
+    let dollars = userCurrency.toUpperCase()
+    if (dollars === "AUSTRALIAN DOLLARS" || dollars === "AUD") {
       result = "AUD";
-    } else if (searchInput === "Bahamian Dollars") {
+    } else if (dollars === "BAHAMIAN DOLLARS" || dollars === "BSD") {
       result = "BSD";
-    } else if (searchInput === "Canadian Dollars") {
+    } else if (dollars === "CANADIAN DOLLARS" || dollars === "CAD") {
       result = "CAD";
-    } else if (searchInput === "Hong Kong Dollars") {
+    } else if (dollars === "HONG KONG DOLLARS" || dollars === "HKD") {
       result = "HKD";
-    } else if (searchInput === "New Zealand Dollars") {
+    } else if (dollars === "NEW ZEALAND DOLLARS" || dollars === "NZD") {
       result = "NZD";
     } else {
-      result = "XXX";
+      result = null;
     }
     return result;
   }
-  
-  static getElements(response, searchInput) {
-    let currencyObject = response.conversion_rates[searchInput];
+
+  static getElements(response, userCurrency) {
+    let currencyObject = response.conversion_rates[userCurrency];
     console.log(currencyObject);
     return currencyObject;
   }
-  static math(a, b) {
-    if (isNaN(a)) {
-      return a;
-    } else if (isNaN(b)) {
+  static math(selectedExchangeRate, userUSD) {
+    if (isNaN(selectedExchangeRate)) {
+      return selectedExchangeRate;
+    } else if (isNaN(userUSD)) {
       return "Did you input your currency in numbers?";
     } else {
-      return (a * b).toFixed(2);
+      return (selectedExchangeRate * userUSD).toFixed(2);
     }
   }
 }
