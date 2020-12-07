8a5754fb1d060ce5a55d14ca509ab699c8a60b88
Shannon Grantski <grantski@pm.me>
2020-12-07 09:28:16 -0800

    add api-specified error messaging

--- src/main.js
+++ src/main.js
@@ -8,17 +8,35 @@ $(document).ready(function() {
     e.preventDefault();
     let userUSD = parseInt($("#usd-input").val());
     let search = $("#currency-select").val();
-
-    // api call
+    
     let request = new XMLHttpRequest();
     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const apiResponse = JSON.parse(this.responseText);
-        let outputQ = Response.getElements(apiResponse, search);
-        let outputZ = Response.math(outputQ, userUSD);
-        alert(outputZ);
+        if (apiResponse.result === "success") {
+          let outputQ = Response.getElements(apiResponse, search);
+          let outputZ = Response.math(outputQ, userUSD);
+          console.log(outputZ);
+        
+        } else if (apiResponse["error-type"] == "unsupported-code") {
+          console.log("This currency code is not supported.");
+        } else if (apiResponse["error-type"] === "base-code-only-on-pro") {
+          console.log("The specified currency code is not available through this application.");
+        } else if (apiResponse["error-type"] === "malformed-request") {
+          console.log("Sorry! There was an error in forming this request.");
+        } else if (apiResponse["error-type"] === "invalid-key") {
+          console.log("The API key is invalid or expired. Sorry!");
+        } else if (apiResponse["error-type"] === "quota-reached") {
+          console.log("Quota of queries reached. Sorry! Please try again later.");
+        } else if (apiResponse["error-type"] === "not-available-on-plan") {
+          console.log("This request not valid via this application.");
+        } else {
+          console.log("Something broke along the way.");
+          console.log(apiResponse["error-type"]);
+          console.log(apiResponse.result);
+        }
       } else {
         return "error";
       }
@@ -28,4 +46,3 @@ $(document).ready(function() {
     request.send();
   });
 });
-
