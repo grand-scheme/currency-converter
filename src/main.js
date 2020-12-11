34bc14404f851568b1477bccfe341bd1741485b9
Shannon Grantski <grantski@pm.me>
2020-12-11 00:37:45 -0800

    delete empty space

--- src/main.js
+++ src/main.js
@@ -3,43 +3,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/styles.css';
-// import GetConversion from './../assets/js/conversion.js';
+import GetConversion from './../assets/js/conversion.js';
 import Errors from '../assets/js/error-handling.js';
 import CallService from '../assets/js/call-service.js';
-
-// // BUSINESS LOGIC: API CALL
-// function makeCall(userCurrency, userUSD) {
-//     let request = new XMLHttpRequest();
-//     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-//     request.onload = function() {
-//       if (this.status === 200) {
-//       let apiOutput = JSON.parse(this.responseText);
-//       if (Errors.noApiErrors(apiOutput) === true) {
-//         // return apiOutput;
-//         GetConversion.getConversion(userCurrency, userUSD, apiOutput);
-//       }
-//     }
-//   };
-//   request.open("GET", url, true);
-//   request.send();
-// }
-
-
-
-
-
-
-
-
-
 
 function clearFields() {
   $("#error-output").val("");
   $("#content-success").hide();
   $("#content-error").hide();
 }
-
-
 
 function uiConversion(userCurrency, userUSD) {
   let checkDollars = userCurrency.toUpperCase();
@@ -50,10 +22,6 @@ function uiConversion(userCurrency, userUSD) {
   $("#content-success").show();
 }
 
-
-
-
-// User Interface
 $(document).ready(function() {
   $("#converter-input").submit(function(e) {
     e.preventDefault();
@@ -65,11 +33,13 @@ $(document).ready(function() {
     Errors.noMoneyErrors(userUSD);
     Errors.noDollarErrors(userCurrency);
     if (Errors.noDollarErrors(userCurrency) === true && Errors.noMoneyErrors(userUSD) == true) {
-      CallService.makeCall(userCurrency, userUSD);
-      // let apiOutput = makeCall();
-      // GetConversion.getConversion(userCurrency, userUSD, apiOutput);
-      // console.log
-      uiConversion(userCurrency, userUSD);
+      const apiOutput = CallService.makeCall();
+      if (Errors.noApiErrors(apiOutput) === true) {
+        GetConversion.getConversion(userCurrency, userUSD, apiOutput);
+        uiConversion(userCurrency, userUSD);
+      } else {
+        Errors.noApiErrors(apiOutput);
+      }
     }
   });
 });
